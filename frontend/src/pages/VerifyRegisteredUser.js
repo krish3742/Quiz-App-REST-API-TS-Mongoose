5669f9658845ecf5a9b433a559a843ecfe90992a
krish3742 <krish3742@gmail.com>
2024-07-19 21:17:41 +0530

    Designed VerifyRegisteredUser Page

--- frontend/src/pages/VerifyRegisteredUser.js
+++ frontend/src/pages/VerifyRegisteredUser.js
@@ -1,6 +1,55 @@
+import { Link } from 'react-router-dom';
+import Style from './VerifyRegisteredUser.module.css';
+
 function VerifyRegisteredUser() {
     return (
-        <div>Hello</div>
+        <>
+            <div className={Style.container}>
+                <h2 className={Style.title}>Quiz App</h2>
+                <button className={Style.LoginButton}><Link to='/login' className={Style.link}>Login</Link></button>
+            </div>
+            <div className={Style.linear}>
+                <div className={Style.body}>
+                    <h2 className={Style.heading}>Register yourself!</h2>
+                    <div className={Style.otpPara}>
+                        Enter OTP
+                    </div>
+                    <div className={Style.inputDiv}>
+                        <div>
+                            <label htmlFor='OTP'></label>
+                            <input type='text' id='otp1' className={Style.input}></input>
+                        </div>
+                        <div>
+                            <label htmlFor='OTP'></label>
+                            <input type='text' id='otp2' className={Style.input}></input>
+                        </div>
+                        <div>
+                            <label htmlFor='OTP'></label>
+                            <input type='text' id='otp3' className={Style.input}></input>
+                        </div>
+                        <div>
+                            <label htmlFor='OTP'></label>
+                            <input type='text' id='otp4' className={Style.input}></input>
+                        </div>
+                        <div>
+                            <label htmlFor='OTP'></label>
+                            <input type='text' id='otp5' className={Style.input}></input>
+                        </div>
+                        <div>
+                            <label htmlFor='OTP'></label>
+                            <input type='text' id='otp6' className={Style.input}></input>
+                        </div>
+                    </div>
+                    <div className={Style.paraDiv}>
+                        <p className={Style.para}>Note: An OTP has been sent on your email. Please verify.</p>
+                    </div>
+                    <button type='submit' className={Style.RegisterButton}>Verify</button>
+                </div>
+                <div className={Style.imgDiv}>
+                    <div className={Style.img}></div>
+                </div>
+            </div>
+        </>
     )
 };
 
